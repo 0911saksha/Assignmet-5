d811749633667c07ae75f9c51def16e596dbf178
0911saksha <108914640+0911saksha@users.noreply.github.com>
2023-03-02 15:32:30 +0530

    Add files via upload

---
+++ first and last string.py
@@ -0,0 +1,10 @@
+inputString = "f"
+
+count = 0
+
+for i in inputString:
+	count = count + 1
+newString = inputString[ 0:2 ] + inputString [count - 2: count ]
+
+print("Input string = " + inputString)
+print("New String = "+ newString)
